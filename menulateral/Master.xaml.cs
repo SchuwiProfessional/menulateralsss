2ee0bfc97bef68f6622a69e0fb2692419988ab33
ESTUDIANTE-ETI <ESTUDIANTE-ETI@LAB02-15>
2022-09-15 18:09:00 -0500

    Agregar archivos de proyecto.

---
+++ menulateral/Master.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace menulateral
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : ContentPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAcerca_Clicked(object sender, EventArgs e)
+        {
+            App.MasterDet.IsPresented = false;
+            await App.MasterDet.Detail.Navigation.PushAsync(new acerca());
+        }
+
+        private async void btnproductos_Clicked(object sender, EventArgs e)
+        {
+            App.MasterDet.IsPresented = false;
+            await App.MasterDet.Detail.Navigation.PushAsync(new Productos());
+        }
+
+        private async void btnventas_Clicked(object sender, EventArgs e)
+        {
+            App.MasterDet.IsPresented = false;
+            await App.MasterDet.Detail.Navigation.PushAsync(new ventas());
+        }
+    }
+}
